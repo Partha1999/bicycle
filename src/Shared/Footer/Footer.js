19022d950fd4f5882fd6d786b5cb8d1168a202bf
Partha1999 <impartha1999@gmail.com>
2022-12-23 00:51:46 +0600

    footer changed 2

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -13,9 +13,7 @@ const Footer = () => {
               href="#"
               className="flex justify-center space-x-3 lg:justify-start"
             >
-              <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-400">
-                <img src={img1} alt="" />
-              </div>
+              
               <span className="self-center text-2xl font-semibold">
                 Bicycle Surfers
               </span>
