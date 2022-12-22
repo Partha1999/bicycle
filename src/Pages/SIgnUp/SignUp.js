3b68fd2e3767da1f278fb36b8ca074ad7e8056ab
Partha1999 <impartha1999@gmail.com>
2022-12-23 00:47:30 +0600

    sign up finished

--- src/Pages/SIgnUp/SignUp.js
+++ src/Pages/SIgnUp/SignUp.js
@@ -4,6 +4,10 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import img from "../../Images/signup.webp";
 
+const handelGoogleSignUp = (signInWithGoogle, location, navigate) => {
+  const role = "buyer";
+  signInWithGoogle(role, location, navigate);
+};
 
 const SignUp = () => {
   const {
